6fde383cc4c5a7f67bf170f913913fc4218d277e
Eunsu-Joo <holicholicpop@gmail.com>
2022-03-24 18:38:20 +0900

    ◾ 제발 되라..

--- src/api/event/controllers/event.js
+++ src/api/event/controllers/event.js
@@ -54,11 +54,11 @@ module.exports = createCoreController("api::event.event", ({ strapi }) => ({
   },
   async me(ctx) {
     const user = ctx.state.user;
-    if (!user) {
-      return ctx.badRequest(null, [
-        { message: "No authorization header was found" },
-      ]);
-    }
+    // if (!user) {
+    //   return ctx.badRequest(null, [
+    //     { message: "No authorization header was found" },
+    //   ]);
+    // }
 
     const data = await strapi.db.query("api::event.event").findMany({
       where: { users_permissions_user: { id: user.id } },
